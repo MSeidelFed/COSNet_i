be6b165383eef294410ff8c43d95fb4481f12ec0
Yin <hsieh.y.chen@uit.no>
2020-09-11 09:56:09 +0200

    update calculate distance with argparse

--- Python_Modules/calculate_distance.py
+++ Python_Modules/calculate_distance.py
@@ -18,6 +18,7 @@ from sys import argv
 from Bio.PDB import PDBParser
 import numpy as np
 import os
+import argparse
 ## FUNCTIONS
 def get_struct(PDBfilepath):
     """
@@ -37,7 +38,7 @@ def get_struct(PDBfilepath):
 
     return structure, struct_id, ent_id, residues
 
-def center_of_mass(one_residue, flag):
+def center_of_mass(one_residue, flag=0):
     """
     Finds the center of mass of a residue
     Returns this as (x,y,z)
@@ -77,46 +78,42 @@ def calc_dist_matrix(entity1, entity2):
             dist_mtx[i,j] = calc_resi_dist(coords_one, coords_two)
     return dist_mtx
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(usage="python3 %(prog)s [-h] <pathto/PDBfile1> <pathto/PDBfile2> <outpath>",
+                                     description="Calculates Euclidean distances between residues of two PDB files, stores in distance matrix file.")
+    parser.add_argument("pdbfile1",help="First PDB file",type=str)
+    parser.add_argument("pdbfile2",help="Second PDB file",type=str)
+    parser.add_argument("outpath",help="Where to output distance matrix file",type=str)
+    args = parser.parse_args()
+    return args
+
 ## MAIN
 if __name__ == "__main__":
     #usage
-    if len(argv) != 4:
-        print('################################################################################################\n')
-        print('     Usage: python3 calculate_distance.py [pathtoPDBfile1] [pathtoPDBfile2] [outputpath]        \n')
-        print('################################################################################################\n')
-    else:
-        #read in comparison files
-        ent1_file = argv[1]
-        ent2_file = argv[2]
-        outputpath = argv[3]
-        if outputpath == ".":
-            outputpath = os.getcwd()
-        coms_method = 0
+    Args = parse_arguments()
+    #parse out structures and info of both files
+    Ent1Struct, Struct1ID, Ent1ID, Residues1 = get_struct(Args.pdbfile1)
+    Ent2Struct, Struct2ID, Ent2ID, Residues2 = get_struct(Args.pdbfile2)
 
-        #parse out structures and info of both files
-        Ent1Struct, Struct1ID, Ent1ID, Residues1 = get_struct(ent1_file)
-        Ent2Struct, Struct2ID, Ent2ID, Residues2 = get_struct(ent2_file)
+    #calculate center of masses for all residues
+    ComsList1 = []
+    for res in Residues1:
+        com1 = center_of_mass(res)
+        ComsList1.append(com1)
 
-        #calculate center of masses for all residues
-        ComsList1 = []
-        for res in Residues1:
-            com1 = center_of_mass(res, coms_method)
-            ComsList1.append(com1)
+    ComsList2 = []
+    for res in Residues2:
+        com2 = center_of_mass(res)
+        ComsList2.append(com2)
 
-        ComsList2 = []
-        for res in Residues2:
-            com2 = center_of_mass(res, coms_method)
-            ComsList2.append(com2)
-
-        DistMtx = calc_dist_matrix(ComsList1, ComsList2)
+    DistMtx = calc_dist_matrix(ComsList1, ComsList2)
     
-        #save distance matrix into output file
-        if Struct1ID != Struct2ID:
-            np.savetxt('dist_mtx_{}_{}.csv'.format(Struct1ID,Struct2ID), DistMtx, delimiter=",")
-            outfile = f'dist_mtx_{Struct1ID}_{Struct2ID}.csv'
-            os.rename(outfile, os.path.join(outputpath, outfile))
-        else:
-            np.savetxt('dist_mtx_{}_{}.csv'.format(Ent1ID,Ent2ID), DistMtx, delimiter=",")
-            outfile = f'dist_mtx_{Ent1ID}_{Ent2ID}.csv'
-            os.rename(outfile, os.path.join(outputpath, outfile))
-            
+    #save distance matrix into output file
+    if Struct1ID != Struct2ID:
+        np.savetxt('dist_mtx_{}_{}.csv'.format(Struct1ID,Struct2ID), DistMtx, delimiter=",")
+        outfile = f'dist_mtx_{Struct1ID}_{Struct2ID}.csv'
+        os.rename(outfile, os.path.join(Args.outpath, outfile))
+    else:
+        np.savetxt('dist_mtx_{}_{}.csv'.format(Ent1ID,Ent2ID), DistMtx, delimiter=",")
+        outfile = f'dist_mtx_{Ent1ID}_{Ent2ID}.csv'
+        os.rename(outfile, os.path.join(Args.outpath, outfile))
