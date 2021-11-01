088c1b0da2c4da7743e4001c50668bb38c8831fc
Yin <hsieh.y.chen@uit.no>
2021-11-01 16:50:11 +0100

    updated docstrings combination

--- Python_Modules/combination.py
+++ Python_Modules/combination.py
@@ -1,25 +1,31 @@
-#!usr/env/bin/ python3
+#!/usr/bin/env python3
 """
-combination.py: 
+[combination.py] 
 
-Reads in file with list of file names,  
-Outputs another file  with all n-combinations of original files,
-one combination per line.
+USAGE: python3 combination.py listoffiles combnum outputfile outpath
 
-Useful for preparing protein pdb file input for pair-wise contact calculation.
-
-python3 combination.py [listoffiles] [combnum] [outputfile] [outpath]
-
+This script is a command-line interface to make n-combinations of 
+files. It is useful for preparing protein pdb file input for pair-wise
+contact calculation. 
 """
-## IMPORTS
+
 from sys import argv
+from pathlib import Path
 import itertools
-import os
 import argparse
-## FUNCTIONS
-def get_list(infile):
+
+def get_list_of_files(infile):
     """
-    Puts lines in file in a list
+    Makes list of file names 
+
+    Parameters
+    ----------
+    infile: pathlib.PosixPath
+
+    Returns
+    -------
+    filelist: list
+        list of file names
     """
     filelist = []
     with open(infile) as f:
@@ -30,13 +36,37 @@ def get_list(infile):
 
 def combinate(listoffiles, combnum):
     """
-    Makes a list of all the N choose combnum options
+    Makes a list of tuples with the N choose combnum options
+
+    Parameters
+    ----------
+    listoffiles: list
+        list of file names
+    combnum: int
+        combination number (N choose combnum)
+
+    Returns
+    -------
+    combinedlist: list
+        list of tuples with combnum-combinations of files
     """
     combinedlist = list(itertools.combinations(listoffiles, combnum))
     return combinedlist
 
 def print_output(combinedlist, filename, outpath):
-    filename = os.path.join(outpath,filename)
+    """
+    Writes out file with combinations of files per line
+
+    Parameters
+    ----------
+    combinedlist: list
+        list of tuples with file combinations
+    filename: str
+        name of file to write file combinations
+    outpath: pathlib.PosixPath
+        path to output file
+    """
+    filename = outpath.joinpath(filename)
     outfile = open(filename, 'w+')
     for item in combinedlist:
         ent = " ".join(item)
@@ -44,7 +74,7 @@ def print_output(combinedlist, filename, outpath):
     outfile.close()
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(usage="python3 %(prog)s [-h] <inputfile> <N> <outfile> <outpath",
+    parser = argparse.ArgumentParser(usage="python3 %(prog)s [-h] inputfile N outfile outpath",
                                      description="Returns file of N-wise combinations of input file list.")
     parser.add_argument("inputfile", help="One column text file of filenames", type=str)
     parser.add_argument("N", help="Combination number, e.g. 2 gives pair-wise combinations", type=int)
@@ -55,15 +85,18 @@ def parse_arguments():
 
 def main():
     Args = parse_arguments()
-    FileList = get_list(Args.inputfile)
-    if Args.N == 1:
-        print(f'Combination of 1 is just your input file: {Args.inputfile}')
-    elif Args.N > len(FileList):
-        print(f'Combination number: {Args.N} cannot be larger than number of files inputted: {len(FileList)}')
+    inputfile = Path(Args.inputfile)
+    if not inputfile.is_file():
+        raise FileNotFoundError('Check your input file!')
     else:
-        CombList = combinate(FileList,Args.N)
-        print_output(CombList,Args.outfile,Args.outpath)
+        FileList = get_list_of_files(inputfile)
+        if Args.N == 1:
+            print(f'Combination of 1 is just your input file: {Args.inputfile}')
+        elif Args.N > len(FileList):
+            print(f'Combination number: {Args.N} cannot be larger than number of files inputted: {len(FileList)}')
+        else:
+            CombList = combinate(FileList,Args.N)
+            print_output(CombList,Path(Args.outfile),Path(Args.outpath))
     
-## MAIN
 if __name__=="__main__":
     main()
